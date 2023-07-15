fa606a8b4b60ccf99aed72c56b65323240b60519
viktor <gictorbit@gmail.com>
2023-07-15 08:58:24 +0330

    update user table

--- migrations/sqls/20230714103105_init.sql
+++ migrations/sqls/20230714103105_init.sql
@@ -5,7 +5,7 @@ CREATE TABLE users
     id         SERIAL PRIMARY KEY,
     first_name VARCHAR(255)        NOT NULL,
     last_name  VARCHAR(255),
-    user_name  VARCHAR(255) UNIQUE,
+    user_name  VARCHAR(255) UNIQUE NOT NULL ,
     email      VARCHAR(255) UNIQUE NOT NULL,
     password   VARCHAR(255)        NOT NULL,
     role       INTEGER             NOT NULL DEFAULT 0,
