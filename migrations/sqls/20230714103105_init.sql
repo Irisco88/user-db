ac321e38bc2bcc85668e7d859d3b6b232f2b394f
viktor <gictorbit@gmail.com>
2023-07-15 08:43:00 +0330

    add user table

--- migrations/sqls/20230714103105_init.sql
+++ migrations/sqls/20230714103105_init.sql
@@ -1,9 +1,20 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE users
+(
+    id         SERIAL PRIMARY KEY,
+    first_name VARCHAR(255)        NOT NULL,
+    last_name  VARCHAR(255),
+    user_name  VARCHAR(255) UNIQUE,
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
+    role       INTEGER             NOT NULL DEFAULT 0,
+    created_at TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DROP TABLE users;
 -- +goose StatementEnd
