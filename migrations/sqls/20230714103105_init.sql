4a6fbbe4803e999eada7472e4e8980fb59237e7e
viktor <gictorbit@gmail.com>
2023-07-23 10:04:47 +0330

    update user table schema

--- migrations/sqls/20230714103105_init.sql
+++ migrations/sqls/20230714103105_init.sql
@@ -3,18 +3,22 @@
 CREATE TABLE users
 (
     id         SERIAL PRIMARY KEY,
-    first_name VARCHAR(255)        NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
     last_name  VARCHAR(255),
-    user_name  VARCHAR(255) UNIQUE NOT NULL ,
-    email      VARCHAR(255) UNIQUE NOT NULL,
-    password   VARCHAR(255)        NOT NULL,
-    role       INTEGER             NOT NULL DEFAULT 0,
-    created_at TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP
+    user_name  VARCHAR(255) NOT NULL,
+    email      VARCHAR(255),
+    password   VARCHAR(255) NOT NULL,
+    role       INTEGER      NOT NULL DEFAULT 0,
+    avatar     VARCHAR(500),
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE UNIQUE INDEX idx_users_unique_email ON users (email);
+CREATE UNIQUE INDEX idx_users_unique_user_name ON users (user_name);
 
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE users;
+DROP TABLE IF EXISTS users;
 -- +goose StatementEnd
