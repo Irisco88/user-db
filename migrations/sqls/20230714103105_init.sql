dccd271228337fba835c5a842e2f636c51747112
viktor <gictorbit@gmail.com>
2023-07-23 10:50:16 +0330

    add owner id to users

--- migrations/sqls/20230714103105_init.sql
+++ migrations/sqls/20230714103105_init.sql
@@ -10,6 +10,7 @@ CREATE TABLE users
     password   VARCHAR(255) NOT NULL,
     role       INTEGER      NOT NULL DEFAULT 0,
     avatar     VARCHAR(500),
+    owner_id   INTEGER      NOT NULL DEFAULT 0,
     created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
